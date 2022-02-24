4ac2b3f8e5f6745dc12789bfeceef89c7972501a
Blai Fernández Bravo <94699337+blass53@users.noreply.github.com>
2022-02-24 22:21:37 +0100

    fsdfsfdsf

---
+++ js/logjs.js
@@ -0,0 +1,48 @@
+const nombre = document.getElementById("myname");
+const apellidos = document.getElementById("surname");
+const correo = document.getElementById("email");
+const celular = document.getElementById("mobile");
+const contrasenia = document.getElementById("password");
+const contrasenia2 = document.getElementById("repeatPassword");
+const terminosYcondiciones = document.getElementById("termsAndConditions");
+const form = document.getElementById("form");
+const listInputs = document.querySelectorAll(".form-input");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let condicion = validacionForm();
+  if (condicion) {
+    enviarFormulario();
+  }
+});
+
+function validacionForm() {
+  form.lastElementChild.innerHTML = "";
+  let condicion = true;
+  listInputs.forEach((element) => {
+    element.lastElementChild.innerHTML = "";
+  });
+
+
+  if (correo.value.length < 5 || correo.value.trim() == "") {
+    mostrarMensajeError("email", "Correo no valido*");
+    condicion = false;
+  }
+  if (contrasenia.value.length < 7 || contrasenia.value.trim() == "") {
+    mostrarMensajeError("password", "Contraseña no valido*");
+    condicion = false;
+  }
+ else {
+  return condicion;
+  }
+}
+
+function mostrarMensajeError(claseInput, mensaje) {
+  let elemento = document.querySelector(`.${claseInput}`);
+  elemento.lastElementChild.innerHTML = mensaje;
+}
+
+function enviarFormulario() {
+  form.reset();
+  form.lastElementChild.innerHTML = "Listo !!";
+}
